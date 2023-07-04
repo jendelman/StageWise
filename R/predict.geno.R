3ef87bb7d4f63531237f0c7d307d218282ef1e49
jendelman <endelman@wisc.edu>
2023-07-04 14:23:23 -0500

    v1.03

--- R/predict.geno.R
+++ R/predict.geno.R
@@ -16,12 +16,14 @@ NULL
 setGeneric("predict")
 setMethod("predict",c(object="class_geno"),
           definition=function(object, marker.effects){
-            stopifnot(marker.effects$marker==colnames(object@coeff))
+            stopifnot(marker.effects$marker %in% colnames(object@coeff))
             if (attr(marker.effects,"what")=="AM") {
-              pred <- as.numeric(object@coeff %*% matrix(marker.effects$effect,ncol=1))
+              pred <- as.numeric(object@coeff[,marker.effects$marker] 
+                                 %*% matrix(marker.effects$effect,ncol=1))
             } else {
               stopifnot(class(object)=="class_genoD")
-              pred <- as.numeric(object@coeff.D %*% matrix(marker.effects$effect,ncol=1))
+              pred <- as.numeric(object@coeff.D[,marker.effects$marker] 
+                                 %*% matrix(marker.effects$effect,ncol=1))
             }
             data.frame(id=rownames(object@coeff), value=pred)
           })
